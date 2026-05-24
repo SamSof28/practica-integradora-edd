e903fbca76591bd486b6af6e49cf61661460b166
SamSof28 <wassamuel@gmail.com>
2026-05-24 11:46:51 -0500

    refactor: replace hardcoded data with dynamic file loading for improved flexibility

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from src.engine import DocumentCollection
+import json
+import os
+
+os.system("clear")
 
 # 1. Instanciar la colección
 collection = DocumentCollection()
 
-# 2. Cargar datos estructurados
-data = [
-    {
-        "id": 1,
-        "nombre": "Ana",
-        "edad": 25,
-        "ciudad": "Medellín",
-        "direccion": { "barrio": "Laureles" }
-    },
-    {
-        "id": 1,
-        "nombre": "Ana",
-        "edad": 25,
-        "ciudad": "Medellín",
-        "direccion": { "barrio": "Laureles" }
-    }
-]
+def leer_archivo(ruta):
+    with open(ruta, 'r', encoding='utf-8') as archivo:
+        # .load (sin la 's') lee un objeto de archivo
+        datos = json.load(archivo) 
+    return datos # Esto devuelve una lista o dict de Python
+
+
+data = leer_archivo("data/ejemplo1.json")
 collection.load(data)
+
+print(collection.find({
+"ciudad": "Medellín",
+"edad": {"$gte": 50}
+}))
+print("----"*5)
 
 print(collection)
